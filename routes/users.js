a80fa78663263d3d20581ac583dc10abcc38e972
ShannonGW <ShannonWilliams93@gmail.com>
2022-05-11 18:09:16 +0200

    GET users and tasks

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,44 @@
 var express = require("express");
 var router = express.Router();
+const db = require("../model/helper");
 
-/* GET users listing. */
+// router.get("/", (req, res) => {
+//   res.send("Welcome to MatchUp!");
+// });
+
+//  GET user list
 router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+  db("SELECT * FROM Users;")
+    .then((results) => {
+      res.send(results.data);
+    })
+    .catch((err) => res.status(500).send(err));
+});
+
+//  GET task list
+router.get("/tasks", function (req, res, next) {
+  db("SELECT * FROM Tasks;")
+    .then((results) => {
+      res.send(results.data);
+    })
+    .catch((err) => res.status(500).send(err));
 });
 
 module.exports = router;
+
+// // GET one student
+// router.get("/:id", function (req, res, next) {
+//   //your code here
+// });
+
+// // INSERT a new student into the DB
+// router.post("/", function (req, res, next) {
+//   //your code here
+// });
+
+// // DELETE a student from the DB
+// router.delete("/:id", function (req, res, next) {
+//   //your code here
+// });
+
+// module.exports = router;
